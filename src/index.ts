fe5726efd76f11a38608477e6008cbaad1473f6e
Jonathan <premiersoft-jonathan.reichel@ailos.coop>
2026-07-16 23:15:13 -0300

    feat(build) added build options

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+export * from './tokens';
+
+// atoms
+export { Button } from './components/atoms/Button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './components/atoms/Button';
+
+export { Input } from './components/atoms/Input';
+export type { InputProps, InputType } from './components/atoms/Input';
+
+export { Label } from './components/atoms/Label';
+export type { LabelProps, LabelSize } from './components/atoms/Label';
+
+export { Select } from './components/atoms/Select';
+export type { SelectProps, SelectSize } from './components/atoms/Select';
+
+export { IconButton } from './components/atoms/IconButton';
+export type { IconButtonProps, IconButtonVariant, IconButtonSize } from './components/atoms/IconButton';
+
+export { TrendIndicator } from './components/atoms/TrendIndicator';
+export type { TrendIndicatorProps, TrendDirection } from './components/atoms/TrendIndicator';
+
+export { Toast } from './components/atoms/Toast';
+export type { ToastProps, ToastVariant } from './components/atoms/Toast';
+
+export { NavItem } from './components/atoms/NavItem';
+export type { NavItemProps } from './components/atoms/NavItem';
+
+export { EmptyState } from './components/atoms/EmptyState';
+export type { EmptyStateProps } from './components/atoms/EmptyState';
+
+// molecules
+export { FormField } from './components/molecules/FormField';
+export type { FormFieldProps } from './components/molecules/FormField';
+
+export { FormFieldSelect } from './components/molecules/FormFieldSelect';
+export type { FormFieldSelectProps } from './components/molecules/FormFieldSelect';
+
+export { StatCard } from './components/molecules/StatCard';
+export type { StatCardProps, StatCardTone, StatCardTrend } from './components/molecules/StatCard';
+
+export { PedidoSummaryRow } from './components/molecules/PedidoSummaryRow';
+export type { PedidoSummaryRowProps } from './components/molecules/PedidoSummaryRow';
+
+export { PedidoListItem } from './components/molecules/PedidoListItem';
+export type { PedidoListItemProps } from './components/molecules/PedidoListItem';
+
+export { ProdutoListItem } from './components/molecules/ProdutoListItem';
+export type { ProdutoListItemProps } from './components/molecules/ProdutoListItem';
+
+export { PercentualListItem } from './components/molecules/PercentualListItem';
+export type { PercentualListItemProps } from './components/molecules/PercentualListItem';
+
+// organisms
+export { OnboardingCard } from './components/organisms/OnboardingCard';
+export type { OnboardingCardProps, OnboardingAction } from './components/organisms/OnboardingCard';
+
+export { Sidebar } from './components/organisms/Sidebar';
+export type { SidebarProps, SidebarScreen } from './components/organisms/Sidebar';
+
+export { DashboardSummary } from './components/organisms/DashboardSummary';
+export type { DashboardSummaryProps, DashboardTrend } from './components/organisms/DashboardSummary';
+
+export { PedidosList } from './components/organisms/PedidosList';
+export type { PedidosListProps, MesOption } from './components/organisms/PedidosList';
+
+export { ProdutosList } from './components/organisms/ProdutosList';
+export type { ProdutosListProps } from './components/organisms/ProdutosList';
+
+export { PercentuaisList } from './components/organisms/PercentuaisList';
+export type { PercentuaisListProps } from './components/organisms/PercentuaisList';
+
+export { PercentualForm } from './components/organisms/PercentualForm';
+export type { PercentualFormProps } from './components/organisms/PercentualForm';
+
+export { ProdutoForm } from './components/organisms/ProdutoForm';
+export type { ProdutoFormProps, PercentualOption } from './components/organisms/ProdutoForm';
+
+export { PedidoForm } from './components/organisms/PedidoForm';
+export type {
+  PedidoFormProps,
+  ProdutoOption,
+  PercentualOption as PedidoFormPercentualOption,
+} from './components/organisms/PedidoForm';
+
+// templates
+export { AppShellTemplate } from './components/templates/AppShellTemplate';
+export type { AppShellTemplateProps } from './components/templates/AppShellTemplate';
